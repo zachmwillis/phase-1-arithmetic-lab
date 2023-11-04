b680310c6be4491aa152f84d6ac7350b6aca6e04
Zach Willis <zachmw27@gmail.com>
2023-11-03 23:37:29 -0400

    complete

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) - 1;
+
+let num3 = 8;
+let num4 = 2;
+let mod = num3 / num4;
+
+let max = (5, 10, 15, 20);
+Math.max(...max);
